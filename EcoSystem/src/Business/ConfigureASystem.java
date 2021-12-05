9161ae81f239294c50eaf9f804c79a5e36bf8617
Chandana Harish <harish.c@northeastern.edu>
2021-12-05 18:24:42 -0500

    Project Skeleton

---
+++ EcoSystem/src/Business/ConfigureASystem.java
@@ -0,0 +1,18 @@
+package Business;
+
+
+/**
+ *
+ * @author ajay09
+ */
+public class ConfigureASystem {
+    
+    public static EcoSystem configure(){
+        
+        
+        return null;
+        
+        
+    }
+    
+}
