a615f8d68cad6d5d6d3b39faeeb7722277a9a2b8
Prerana Jagannatha Reddy <jagannathareddy.p@northeastern.edu>
2021-12-06 13:52:00 -0500

    Oncologist class added

--- EcoSystem/src/Business/WorkQueue/OncologistAppointment.java
+++ EcoSystem/src/Business/WorkQueue/OncologistAppointment.java
@@ -10,4 +10,79 @@ package Business.WorkQueue;
  */
 public class OncologistAppointment {
     
+    String oncologistName;
+    String lastName;
+    String status;
+    int oncologistID;
+    String userName;
+    String userID;
+    String Time;
+    String Date;
+
+    public String getOncologistName() {
+        return oncologistName;
+    }
+
+    public void setOncologistName(String oncologistName) {
+        this.oncologistName = oncologistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getOncologistID() {
+        return oncologistID;
+    }
+
+    public void setOncologistID(int oncologistID) {
+        this.oncologistID = oncologistID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String Time) {
+        this.Time = Time;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+    
+    
+    
 }
