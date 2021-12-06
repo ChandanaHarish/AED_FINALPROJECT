8b773653f1cf495453e740967766a180b2be4127
Chandana Harish <harish.c@northeastern.edu>
2021-12-06 08:38:00 -0500

    Added ambulance directory

---
+++ EcoSystem/src/Business/Ambulance/ambulance_directory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Ambulance;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author chand
+ */
+public class ambulance_directory {
+     ArrayList<Ambulance> amb =new ArrayList<Ambulance>();
+    
+    public void addambulance(Ambulance c)
+    {        
+       amb.add(c);
+    }
+
+    public ArrayList<Ambulance> getA() {
+        return amb;
+    }
+
+    public void setA(ArrayList<Ambulance> amb) {
+        this.amb = amb;
+    }
+
+    
+    
+    public void removeambulance(Ambulance customer){
+        amb.remove(customer);
+    }
+    
+}
