978ec0a74619679ee72b42f8dcf5302f79e6013d
Ajay Arumugam <arumugam.a@northeastern.edu>
2021-12-09 20:09:22 -0500

    Working on EcoSystem

--- EcoSystem/src/Business/ConfigureASystem.java
+++ EcoSystem/src/Business/ConfigureASystem.java
@@ -1,6 +1,9 @@
 package Business;
 
+import Business.Employee.Employee;
 import Business.UserAccount.UserAccount;
+import static java.time.Clock.system;
+import Business.role.SystemAdminRole;
 
 
 /**
@@ -12,10 +15,15 @@ public class ConfigureASystem {
     public static EcoSystem configure(){
         
         
+        EcoSystem system = EcoSystem.getInstance();
         
-        return null;
+        //Employee employee = system.getEmployeeDirectory().createEmployee("RRH");
+        
+        //UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }     
         
         
     }
     
-}
--- EcoSystem/src/Business/EcoSystem.java
+++ EcoSystem/src/Business/EcoSystem.java
@@ -6,8 +6,11 @@
 package Business;
 
 import Business.WorkQueue.OrderRequestDirectory;
-
-
+import Business.user.userDirectory;
+import Business.role.Role;
+import java.util.ArrayList;
+import Business.role.Role;
+import Business.role.SystemAdminRole;
 
 
 
@@ -15,12 +18,41 @@ import Business.WorkQueue.OrderRequestDirectory;
  *
  * @author ajay09
  */
-public class EcoSystem {
+public class EcoSystem extends Organization{
+    
+    private static EcoSystem business;
 
-    public OrderRequestDirectory getOrderRequestDirectory() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //public EcoSystem (userDirectory userdirectory) {
+       // this.userdirectory = userdirectory;
+    //}
+//    public OrderRequestDirectory getOrderRequestDirectory() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+    
+    public static EcoSystem getInstance(){
+        if(business==null){
+            business=new EcoSystem();
+        }
+        return business;
+    }
+    
+     private EcoSystem(){
+        super(null);
+       
+    }
+ 
+    
+    public boolean checkIfUserIsUnique(String userName){
+       //
+       return false;
     }
 
-   
+//    Object getUserAccountDirectory() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+
+    
+
+    
    
 }
