6e42e7aee83b428bb4c536d5eb5e04c89e9eb0fc
Prerana Jagannatha Reddy <jagannathareddy.p@northeastern.edu>
2021-12-11 23:52:32 -0500

    Fixed bugs in UI pages

--- EcoSystem/src/userInterface/Pharmacy/ManageMedicines.java
+++ EcoSystem/src/userInterface/Pharmacy/ManageMedicines.java
@@ -7,8 +7,8 @@ package userInterface.Pharmacy;
 
 import Business.DB4OUtil.DB4OUtil;
 import Business.EcoSystem;
-import Business.Pharmacy.Pharmacy;
 import Business.UserAccount.UserAccount;
+import Business.Pharmacy.Pharmacy;
 import java.util.Map;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -16,30 +16,25 @@ import javax.swing.table.DefaultTableModel;
 
 /**
  *
- * @author chand
+ * @author raksh
  */
-
-
-    /**
-     * Creates new form ManageMedicines
-     */
-    public class ManageMedicines extends javax.swing.JPanel {
+public class ManageMedicines extends javax.swing.JPanel {
 
     private final JPanel container;
-    private final EcoSystem ecoSystem;
-    private final UserAccount ua;
-    Map<String,String> generalMedicines;
+    private final EcoSystem ecosystem;
+    private final UserAccount userAccount;
+    Map<String,String> medicines;
 
     /**
      * Creates new form manage_medicines
      */
-    public ManageMedicines(JPanel container,EcoSystem ecoSystem,UserAccount ua) {
+    public ManageMedicines(JPanel container,EcoSystem ecosystem,UserAccount userAccount) {
         initComponents();
         this.container=container;
-        this.ecoSystem=ecoSystem;
-        this.ua=ua;
-        Pharmacy ph=(Pharmacy)ua;
-        generalMedicines=ph.getGeneralMedicines();
+        this.ecosystem=ecosystem;
+        this.userAccount=userAccount;
+        Pharmacy p=(Pharmacy)userAccount;
+        medicines=p.getGeneralMedicines();
         populateTable();
 
 
@@ -54,14 +49,27 @@ import javax.swing.table.DefaultTableModel;
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        tblMedicine = new javax.swing.JScrollPane();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        lblMedName = new javax.swing.JLabel();
-        txtMedName = new javax.swing.JTextField();
-        btnMedAdd = new javax.swing.JButton();
-        btnMedDelete = new javax.swing.JButton();
-        txtMedCost = new javax.swing.JTextField();
-        lblMedCost = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(6, 39, 68));
+
+        jLabel3.setFont(new java.awt.Font("Berlin Sans FB", 0, 16)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel3.setText("Name :");
+
+        jLabel4.setBackground(new java.awt.Color(26, 39, 68));
+        jLabel4.setFont(new java.awt.Font("Berlin Sans FB", 0, 16)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel4.setText("Cost :");
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -71,135 +79,139 @@ import javax.swing.table.DefaultTableModel;
                 "Item Name ", "Item Cost"
             }
         ));
-        tblMedicine.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTable1);
 
-        lblMedName.setFont(new java.awt.Font("Berlin Sans FB", 0, 16)); // NOI18N
-        lblMedName.setText("Name :");
-
-        btnMedAdd.setBackground(new java.awt.Color(195, 233, 245));
-        btnMedAdd.setText("Add ");
-        btnMedAdd.setPreferredSize(new java.awt.Dimension(63, 23));
-        btnMedAdd.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnMedAddActionPerformed(evt);
-            }
-        });
-
-        btnMedDelete.setBackground(new java.awt.Color(195, 233, 245));
-        btnMedDelete.setText("Delete");
-        btnMedDelete.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnMedDeleteActionPerformed(evt);
-            }
-        });
-
-        txtMedCost.addKeyListener(new java.awt.event.KeyAdapter() {
+        jTextField2.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
-                txtMedCostKeyPressed(evt);
+                jTextField2KeyPressed(evt);
             }
         });
 
-        lblMedCost.setBackground(new java.awt.Color(26, 39, 68));
-        lblMedCost.setFont(new java.awt.Font("Berlin Sans FB", 0, 16)); // NOI18N
-        lblMedCost.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        lblMedCost.setText("Cost :");
+        jButton1.setBackground(new java.awt.Color(195, 233, 245));
+        jButton1.setText("Add ");
+        jButton1.setPreferredSize(new java.awt.Dimension(63, 23));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setBackground(new java.awt.Color(195, 233, 245));
+        jButton2.setText("Delete");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/pills (1).png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(45, 45, 45)
+                .addContainerGap(180, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 418, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(172, 172, 172))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(329, 329, 329))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(259, 259, 259)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lblMedName)
-                    .addComponent(lblMedCost, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(txtMedCost, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(btnMedDelete))
+                        .addComponent(jButton2))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(txtMedName, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(btnMedAdd, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addContainerGap(388, Short.MAX_VALUE))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addContainerGap()
-                    .addComponent(tblMedicine, javax.swing.GroupLayout.PREFERRED_SIZE, 418, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(277, Short.MAX_VALUE)))
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButton1, jButton2});
+
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(265, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(35, 35, 35)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblMedName)
-                    .addComponent(txtMedName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnMedAdd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtMedCost, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lblMedCost)
-                    .addComponent(btnMedDelete))
-                .addGap(95, 95, 95))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(68, 68, 68)
-                    .addComponent(tblMedicine, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(202, Short.MAX_VALUE)))
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4)
+                    .addComponent(jButton2))
+                .addContainerGap(130, Short.MAX_VALUE))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButton1, jButton2});
+
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnMedAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMedAddActionPerformed
+    private void jTextField2KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField2KeyPressed
         // TODO add your handling code here:
-        if(txtMedName.getText().isEmpty()||txtMedCost.getText().isEmpty())
+        char c=evt.getKeyChar();
+        if(Character.isLetter(c))
         {
-            JOptionPane.showMessageDialog(null, "All fields are mandatory!!");
-
+            jTextField2.setEditable(false);
+            JOptionPane.showMessageDialog(null, "enter number");
         }
         else
         {
-            String a=txtMedName.getText();
-            String b=txtMedCost.getText();
-            generalMedicines.put(a, b);
-            populateTable();
-            DB4OUtil.dB4OUtil.storeSystem(ecoSystem);
+            jTextField2.setEditable(true);
         }
-    }//GEN-LAST:event_btnMedAddActionPerformed
+    }//GEN-LAST:event_jTextField2KeyPressed
 
-    private void btnMedDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMedDeleteActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if(jTextField1.getText().isEmpty()||jTextField2.getText().isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "All fields are mandatory!!");
+        
+        }
+        else
+        {
+            String a=jTextField1.getText();
+        String b=jTextField2.getText();
+        medicines.put(a, b);
+        populateTable();
+        DB4OUtil.dB4OUtil.storeSystem(ecosystem);
+        }
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         DefaultTableModel t2 = (DefaultTableModel) jTable1.getModel();
         int selectedRow=jTable1.getSelectedRow();
         if(selectedRow>=0)
         {
-            String key=t2.getValueAt(selectedRow, 0).toString();
-            generalMedicines.remove(key);
-            populateTable();
-            DB4OUtil.dB4OUtil.storeSystem(ecoSystem);
+        String key=t2.getValueAt(selectedRow, 0).toString();
+        medicines.remove(key);
+        populateTable();
+        DB4OUtil.dB4OUtil.storeSystem(ecosystem);
         }
         else
         {
             JOptionPane.showMessageDialog(null, "Select a row!!");
         }
-    }//GEN-LAST:event_btnMedDeleteActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void txtMedCostKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtMedCostKeyPressed
-        // TODO add your handling code here:
-        char c=evt.getKeyChar();
-        if(Character.isLetter(c))
-        {
-            txtMedCost.setEditable(false);
-            JOptionPane.showMessageDialog(null, "enter number");
-        }
-        else
-        {
-            txtMedCost.setEditable(true);
-        }
-    }//GEN-LAST:event_txtMedCostKeyPressed
-    
     public void populateTable()
     {
                   
@@ -207,10 +219,10 @@ import javax.swing.table.DefaultTableModel;
         
 
         model.setRowCount(0);
-        for (String key: generalMedicines.keySet()) {
+        for (String key: medicines.keySet()) {
                     Object[] row = new Object[5];
                     row[0] = key;
-                    row[1] =  generalMedicines.get(key);
+                    row[1] =  medicines.get(key);
                     
 
                     model.addRow(row);
@@ -219,13 +231,14 @@ import javax.swing.table.DefaultTableModel;
 }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnMedAdd;
-    private javax.swing.JButton btnMedDelete;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JLabel lblMedCost;
-    private javax.swing.JLabel lblMedName;
-    private javax.swing.JScrollPane tblMedicine;
-    private javax.swing.JTextField txtMedCost;
-    private javax.swing.JTextField txtMedName;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
